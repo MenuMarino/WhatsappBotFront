a4867ba5f259891012cedbb5691fb5f1ff563982
MenuMarino <benjamin.diaz@utec.edu.pe>
2022-06-17 14:58:50 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+axios.defaults.baseURL =
+  'https://9b3c-2001-1388-91-61b5-20c1-b4c4-475f-71ee.ngrok.io';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
